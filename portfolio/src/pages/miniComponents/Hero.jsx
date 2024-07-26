01dc8a6560fdc1b7c750077985dbb2cff43b651d
amitabhdas96 <159752672+amitabhdas96@users.noreply.github.com>
2024-07-27 09:04:42 +1200

    ok

--- portfolio/src/pages/miniComponents/Hero.jsx
+++ portfolio/src/pages/miniComponents/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
   useEffect(() => {
     const getMyProfile = async () => {
       const { data } = await axios.get(
-        "http://localhost:4000/api/v1/user/portfolio/me",
+        "https://mern-stack-portfolio-backend-code.onrender.com/api/v1/user/portfolio/me",
         { withCredentials: true }
       );
       setUser(data.user);
@@ -33,12 +33,12 @@ const Hero = () => {
       </div>
       <h1 className="overflow-x-hidden text-[1.3rem] sm:text-[1.75rem] 
       md:text-[2.2rem] lg:text-[2.8rem] tracking-[2px] mb-4">
-        Hey, I'm Amitabh
+        Hey, I'm Zeeshan
       </h1>
       <h1 className="text-tubeLight-effect overflow-x-hidden text-[1.3rem] 
       sm:text-[1.75rem] md:text-[2.2rem] lg:text-[2.8rem] tracking-[15px]">
         <Typewriter
-          words={["PEGA SOLUTION ARCHITECT", "PEGA PRESALES CONSULTANT", "FULLSTACK DEVELOPER"]}
+          words={["FULLSTACK DEVELOPER", "YOUTUBER", "FREELANCER"]}
           loop={50}
           cursor
           typeSpeed={70}
@@ -48,24 +48,24 @@ const Hero = () => {
       </h1>
       <div className="w-fit px-5 py-2 bg-slate-50 rounded-[20px] flex gap-5 
       items-center mt-4 md:mt-8 lg:mt-10">
-        <Link to={"https://www.youtube.com/channel/UCiU7-qGa4lDr0OtyE7egWDw"} target="_blank">
+        <Link to={"https://www.youtube.com/@CodeWithZeeshu"} target="_blank">
           <Youtube className="text-red-500 w-7 h-7"/>
         </Link>
-        <Link to={"https://www.instagram.com/amitaabhdaas?igsh=MTJ6cGVvYmN4dzU0cA=="} target="_blank">
+        <Link to={user.instagramURL} target="_blank">
           <Instagram className="text-pink-500 w-7 h-7" />
         </Link>
-        <Link to={"https://www.facebook.com/asdfggfdsa0987667890?mibextid=ZbWKwL"} target="_blank">
+        <Link to={user.facebookURL} target="_blank">
           <Facebook className="text-blue-800 w-7 h-7" />
         </Link>
-        <Link to={"https://www.linkedin.com/in/amitabh-das-55378428?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"} target="_blank">
+        <Link to={user.linkedInURL} target="_blank">
           <Linkedin className="text-sky-500 w-7 h-7" />
         </Link>
-        {/* <Link to={user.twitterURL} target="_blank">
+        <Link to={user.twitterURL} target="_blank">
           <Twitter className="text-blue-800 w-7 h-7" />
-        </Link> */}
+        </Link>
       </div>
       <div className="mt-4 md:mt-8 lg:mt-10  flex gap-3">
-        <Link to={"https://github.com/amitabhdas96"} target="_blank">
+        <Link to={user.githubURL} target="_blank">
           <Button className="rounded-[30px] flex items-center gap-2 flex-row">
             <span>
               <Github />
